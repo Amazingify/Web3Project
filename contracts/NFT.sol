57cdf1077ddee5cd3ee5a23e5086e01999e5104b
Amazingify <mostafa_saeed@aucegypt.edu>
2023-02-25 23:32:58 +0200

    refactored the code

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,9 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NFT is ERC721 {
-    
-
+contract NFT is ERC721, ReentrancyGuard {
     uint256 public totalSupply = 0;
     uint256 public price = 1 ether; // ether = 1^18;
 
@@ -17,33 +16,37 @@ contract NFT is ERC721 {
     uint256 public limitPerUser = 3;
     mapping(address => uint) public balances;
 
-
     constructor() ERC721("NFT", "nft") {
         lock = false;
     }
 
-    modifier nonReentrant(){
-        require(!lock,"nonReentrant: lock is active");
-        lock = true;
+    modifier mintChecks(
+        uint amount,
+        uint funds,
+        address to
+    ) {
+        require(amount > 0, "mint: amount is 0");
+        require(funds >= price * amount, "mint:funds sent not enough");
+        require(balances[to] + amount <= limitPerUser, "mint: limit reached");
         _;
-        lock = false;
     }
 
-    function mint(address to, uint amount) external payable nonReentrant {
-        require(amount > 0, "mint: amount is 0");
-        require(msg.value >= price * amount, "mint:funds sent not enough");
-        require(balances[to] + amount <= limitPerUser,"mint: limit reached");
-       
-        if (msg.value > price * amount) {
+    function mint(
+        address to,
+        uint amount
+    ) external payable nonReentrant mintChecks(amount, msg.value, to) {
+        _handleRefund(msg.value, price, amount);
+
+        balances[to] += amount;
+
+        for (uint i = 0; i < amount; i++) _mint(to, totalSupply++);
+    }
+
+    function _handleRefund(uint funds, uint price, uint amount) internal {
+        if (funds > price * amount) {
             uint refund = msg.value - price;
             (bool status, ) = (msg.sender).call{value: refund}("");
-            require(status, "mint: refund failed");
+            require(status, "_handleRefund: refund failed");
         }
-
-        for (uint i = 0; i < amount; i++) {
-            balances[to]++;
-            _mint(to, totalSupply); 
-            totalSupply++; 
-        }
-    }              
+    }
 }
